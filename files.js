890629314592e6eb3714bd156e208c85d824882a
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-02-09 14:20:12 -0700

    ad

--- files.js
+++ files.js
@@ -64,5 +64,8 @@ if(fs.existsSync('./docs/deleteme.txt')) {
  })
 }
 
-                            //Streams: (start using data before it has finished loading)
+                            //Streams and Buffers: 
+    // (start using data before it has finished loading)
+    //buffers pass data a small bit at a time through a stream instead of all at once, this is more efficient for large files. (like when youtube buffers videos)
 
+const fs =  require('fs');
---
+++ streams.js
@@ -0,0 +1,5 @@
+                                    //Streams and Buffers: 
+    // (start using data before it has finished loading)
+    //buffers pass data a small bit at a time through a stream instead of all at once, this is more efficient for large files. (like when youtube buffers videos)
+
+const fs =  require('fs');
