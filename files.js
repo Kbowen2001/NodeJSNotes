a76d635047a8333c7c6c8ac61e6d207a5352effc
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-02-09 13:42:46 -0700

    ad

---
+++ files.js
@@ -0,0 +1,12 @@
+    //The File System: You can interact with your file system through the command prompts
+    //the 'fs' stands for file system.
+
+const fs =  require('fs'); 
+
+    //Reading files:
+
+    //Writing files:
+
+    //Directories:
+
+    //Deleting Files:
