d8b99c61193513447ed06727ee9f61205b00b8f8
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-02-09 13:04:13 -0700

    ad

--- global.js
+++ global.js
@@ -1,7 +1,15 @@
-//Global Object//To run in terminal type node global 
+//Global Object//To run in terminal type node global to see all funcitons available to global object.//
 
-console.log(global);
+// console.log(global); 
 
-global.setTimeout(() => {
+
+//this function runs once after 3 seconds//
+setTimeout(() => {
     console.log('in the timeout');
 }, 3000);
+
+//this funciton runs every 1 second//
+const int = setInterval(() => {
+    console.log('in the interval');
+}, 1000);
+
