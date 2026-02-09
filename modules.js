03d8b1a38ec93997781c1a2f058422aa73df2343
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-02-09 13:33:39 -0700

    ad

--- modules.js
+++ modules.js
@@ -1,3 +1,6 @@
 const xyz =  require('./people'); //this links to js files 
 
+//to link to just one portion do 
+    // const {people} = require('./people'); // this will only import the people array from the people.js file. We can also import the ages array by adding it to the destructuring assignment like this: const {people, ages} = require('./people');
+
 console.log(xyz.people,xyz.ages); 
