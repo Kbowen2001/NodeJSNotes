c22d4c3985e19eca843d059265f7e6fbf7f93160
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-02-09 13:29:54 -0700

    ad

--- people.js
+++ people.js
@@ -1,8 +1,12 @@
 const people = ['mario', 'luigi', 'yoshi', 'toad'];
-
+const ages = [20, 25, 30, 35];
 
 
 //  module.exports = people; // this allows us to export the people array to other files. We can also export functions, objects, etc.
 
 console.log(people);
-module.exports = 'hello';
+module.exports = 'hello'; // this will export the string 'hello' instead of the people array. When we import this module in another file, we will get 'hello' instead of the people array.
+
+module.exports = {
+   people, ages
+};
