5acc3eb3d1fa32c8a6b90b8d22de3e179f988fd7
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-02-09 13:38:11 -0700

    ad

--- modules.js
+++ modules.js
@@ -1,12 +1,13 @@
-// const xyz =  require('./people'); //this links to js files 
+    // const xyz =  require('./people'); //this links to js files 
 
-//to link to just one portion do 
-    const {people,ages} = require('./people'); 
+    //to link to just one portion do 
+const {people,ages} = require('./people'); 
     // this will only import the people array from the people.js file. We can also import the ages array by adding it to the destructuring assignment like this: const {people, ages} = require('./people');
 
 console.log(people, ages); 
 
 const os = require('os'); 
-// this is a built-in module that comes with Node.js. It provides operating system-related utility methods and properties.
+    // this is a built-in module that comes with Node.js. It provides operating system-related utility methods and properties.
 
-console.log(os.platform(), os.homedir());
+console.log(os.platform(), os.homedir()); 
+    // this will log the platform and the home directory of the current user. The os.platform() method returns a string identifying the operating system platform, while os.homedir() returns the home directory of the current user.
