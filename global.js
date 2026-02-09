a775967991cdd42e6bcb4b6ccf092520ecd08a3a
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-02-09 13:05:59 -0700

    ad

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
-//Global Object//To run in terminal type node global to see all funcitons available to global object.//
+//Global Object//To run in terminal type node global to see all funcitons available to global object.// to cancel out of a node process, press ctrl + c //
 
 // console.log(global); 
 
@@ -6,6 +6,7 @@
 //this function runs once after 3 seconds//
 setTimeout(() => {
     console.log('in the timeout');
+    clearInterval(int); // will run every second until hits 3 seconds//
 }, 3000);
 
 //this funciton runs every 1 second//
