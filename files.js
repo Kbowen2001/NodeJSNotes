15a47ba3607d1cbd69554b7a58593dc15fe83930
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-02-09 13:55:42 -0700

    ad

--- files.js
+++ files.js
@@ -15,11 +15,17 @@ console.log(data.toString());
 }); 
 
 console.log('last line'); 
-    // this will log before the file is read because the readFile function is asynchronous. This means that the code will continue to run while the file is being read, and the callback function will be called once the file is read.
+    // this will log before the file is read because the readFile function is asynchronous. This means that the code will continue to run while the file is being read, and the callback function will be called once the file is read. basically it will run code from top to bottom but if one takes too long it will just move to the next line to run.  
 
                                 //Writing files:
 fs.writeFile('./docs/blog1.txt', 'Hello World', () => {
     console.log('file was written');
+});
+
+// if the file does not exist, it will be created. If the file does exist, it will be overwritten. To append to a file instead of overwriting it, you can use the fs.appendFile() method like this:
+
+fs.appendFile('./docs/blog1.txt', '\nHello Again', () => {
+    console.log('file was appended');
 });
 
     //Directories:
