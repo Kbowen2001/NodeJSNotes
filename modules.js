d713b6e16a5dc2ac5199d2b2403342806b66507d
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-02-09 13:37:25 -0700

    ad

--- modules.js
+++ modules.js
@@ -1,6 +1,12 @@
-const xyz =  require('./people'); //this links to js files 
+// const xyz =  require('./people'); //this links to js files 
 
 //to link to just one portion do 
-    // const {people} = require('./people'); // this will only import the people array from the people.js file. We can also import the ages array by adding it to the destructuring assignment like this: const {people, ages} = require('./people');
+    const {people,ages} = require('./people'); 
+    // this will only import the people array from the people.js file. We can also import the ages array by adding it to the destructuring assignment like this: const {people, ages} = require('./people');
 
-console.log(xyz.people,xyz.ages); 
+console.log(people, ages); 
+
+const os = require('os'); 
+// this is a built-in module that comes with Node.js. It provides operating system-related utility methods and properties.
+
+console.log(os.platform(), os.homedir());
