035f828085890574e2f57c04e124e71e0240a2ba
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-02-09 14:20:51 -0700

    ad

--- streams.js
+++ streams.js
@@ -3,3 +3,6 @@
     //buffers pass data a small bit at a time through a stream instead of all at once, this is more efficient for large files. (like when youtube buffers videos)
 
 const fs =  require('fs');
+
+const readStream = fs.createReadStream('./docs/blog3.txt', {encoding: 'utf8'});
+    // this creates a readable stream from the file blog3.txt in the docs directory. The encoding option specifies that the data should be read as a string instead of a buffer. If you omit the encoding option, the data will be read as a buffer by default.
