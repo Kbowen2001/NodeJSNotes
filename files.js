c92c9695975c6147e8263c17ee6a7b971d903484
Kiarra Bowen <kiarrabowen2001@gmail.com>
2026-02-09 13:46:01 -0700

    ad

--- files.js
+++ files.js
@@ -4,10 +4,13 @@
 const fs =  require('fs'); 
 
     //Reading files:
+    //'err' is for error
 fs.readFile('./docs/blog1.txt', (err, data) => {
    if(err){
       console.log(err);
    }
+   console.log(data); 
+});
 
     //Writing files:
 
