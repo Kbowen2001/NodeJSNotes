d7292b4a5c58bdd663d42fe705c9693d6e2ba58f
Kbowen2001 <Kiarrabowen2001@gmail.com>
2026-02-23 12:21:39 -0700

    add

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const server = http.createServer((req, res) => {
             res.write(data); //  This is the body of the response that will be sent back to the client (will show in browser).
             res.end(); // this will end the response and send it back to the client.
         }
+    });
 });
    
 server.listen(3000, 'localhost', () => {
